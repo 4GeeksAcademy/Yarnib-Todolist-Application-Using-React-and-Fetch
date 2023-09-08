4bf2350d5263ff835c07c9c8f6b3304a9b6576ea
Yarnib <138246961+Yarnib@users.noreply.github.com>
2023-09-08 22:35:05 +0000

    Update

--- src/js/App.js
+++ src/js/App.js
@@ -1,52 +1,116 @@
-// App.js
-import React, { useState } from 'react';
-import Task from './component/Task';
+import React, { Component,} from 'react';
+import './App.css';
 
 
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      tasks: [],
+      newTask: '',
+      loading: true,
+    };
+  }
 
+  componentDidMount() {
+    this.fetchTasks();
+  }
 
-function App() {
-  const [tasks, setTasks] = useState([]);
-  const [newTask, setNewTask] = useState('');
+  fetchTasks() {
+    fetch('https://playground.4geeks.com/apis/fake/todos/user/Yarnib')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        const tasksArray = Array.isArray(data.tasks) ? data.tasks : [];
+        this.setState({ tasks: tasksArray, loading: false });
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+        this.setState({ loading: false, tasks: [] });
+      });
+  }
 
-  const addTask = () => {
-    if (newTask.trim() === '') return; // Don't add empty tasks
-    setTasks([...tasks, newTask]);
-    setNewTask('');
+  handleInputChange = (event) => {
+    this.setState({ newTask: event.target.value });
   };
 
-  const deleteTask = (index) => {
-    const updatedTasks = [...tasks];
-    updatedTasks.splice(index, 1);
-    setTasks(updatedTasks);
+  handleAddTask = () => {
+    const { tasks, newTask } = this.state;
+    if (newTask.trim() !== '') {
+      const updatedTasks = [...tasks, { label: newTask, done: false }];
+      this.setState({ tasks: updatedTasks, newTask: '' });
+      this.syncTasks(updatedTasks);
+    }
   };
 
-  return (
-    <div className="App">
-      <h1>Task List</h1>
-      <div className="task-input">
-        <input
-          type="text"
-          placeholder="Add a task..."
-          value={newTask}
-          onChange={(e) => setNewTask(e.target.value)}
-          onKeyPress={(e) => {
-            if (e.key === 'Enter') addTask();
-          }}
-        />
-        <button onClick={addTask}>Add</button>
-      </div>
-      {tasks.length === 0 ? (
-        <p>No tasks, add a task</p>
-      ) : (
-        <div className="task-list">
-          {tasks.map((task, index) => (
-            <Task key={index} text={task} onDelete={() => deleteTask(index)} />
-          ))}
+  handleDeleteTask = (index) => {
+    const { tasks } = this.state;
+    const updatedTasks = tasks.filter((task, i) => i !== index);
+    this.setState({ tasks: updatedTasks });
+    this.syncTasks(updatedTasks);
+  };
+
+  handleClearAllTasks = () => {
+    this.setState({ tasks: [] });
+    this.syncTasks([]);
+  };
+
+  syncTasks(tasks) {
+    fetch('https://playground.4geeks.com/apis/fake/todos/user/Yarnib', {
+      method: 'PUT',
+      body: JSON.stringify({ tasks }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log('Tasks synced successfully:', data);
+      })
+      .catch((error) => {
+        console.error('Error syncing tasks:', error);
+      });
+  }
+
+  render() {
+    const { tasks, newTask, loading } = this.state;
+
+    return (
+      <div>
+        <h1>Todo List</h1>
+        <div>
+          <input
+            type="text"
+            placeholder="Add a task"
+            value={newTask}
+            onChange={this.handleInputChange}
+          />
+          <button onClick={this.handleAddTask}>Add</button>
         </div>
-      )}
-    </div>
-  );
+        <ul>
+          {tasks.map((task, index) => (
+            <li key={index}>
+              {task.label}{' '}
+              <button onClick={() => this.handleDeleteTask(index)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+        <button onClick={this.handleClearAllTasks}>Clear All Tasks</button>
+        {loading && <p>Loading...</p>}
+        {tasks.length === 0 && !loading && <p>No tasks, add a task</p>}
+      </div>
+    );
+  }
 }
 
 export default App;
+
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'; // Ensure the import path is correct
-import "../styles/index.css";
+import App from './App';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
